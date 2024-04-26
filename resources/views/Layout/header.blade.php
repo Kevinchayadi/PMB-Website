9fa4932bb1c70e4fe365c2b42542f2df5a93f0df
KecoaxBunting <david.alvyn@binus.ac.id>
2024-04-26 21:20:01 +0700

    Header

--- resources/views/Layout/header.blade.php
+++ resources/views/Layout/header.blade.php
@@ -1,1 +1,25 @@
-
+<link rel="stylesheet" href="{{ asset('css/header.css') }}">
+<div class="header d-flex justify-content-between">
+    <div class="left d-flex">
+        <div class="logo">
+            <img src="{{ asset('picture/Logo Paroki baru 2.png') }}" alt="">
+        </div>
+        <div class="title align-self-center">
+            <div class="name">
+                <h3>Gereja St. Petrus & Paulus</h2>
+            </div>
+            <div class="address">
+                <h5>Paroki Mangga Besar</h4>
+            </div>
+        </div>
+    </div>
+    <div class="right align-self-center">
+        <ul class="d-flex">
+            <li>Home</li>
+            <li>Profil</li>
+            <li>Jadwal</li>
+            <li>Layanan</li>
+            <li>Hubungi Kami</li>
+        </ul>
+    </div>
+</div>
