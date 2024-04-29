a2842376ccff65f740f7face0d904f57e77bffaf
KecoaxBunting <david.alvyn@binus.ac.id>
2024-04-29 22:30:30 +0700

    29.4.24

--- resources/views/Layout/header.blade.php
+++ resources/views/Layout/header.blade.php
@@ -1,20 +1,24 @@
 <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-<div class="header d-flex justify-content-between">
+<div class="header navbar navbar-expand-lg">
     <div class="left d-flex">
         <div class="logo">
             <img src="{{ asset('picture/Logo Paroki baru 2.png') }}" alt="">
         </div>
         <div class="title align-self-center">
             <div class="name">
-                <h3>Gereja St. Petrus & Paulus</h2>
+                <div class="h3">Gereja St. Petrus & Paulus</div>
             </div>
             <div class="address">
-                <h5>Paroki Mangga Besar</h4>
+                <div class="h5">Paroki Mangga Besar</div>
             </div>
         </div>
     </div>
-    <div class="right align-self-center">
-        <ul class="d-flex h5">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="right align-self-center collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav h5">
             <li class="nav-item">
                 <a class="nav-link disabled" href="#">Home</a>
             </li>
