617e0f78f052587a01f10f5ff4411df1cb9b86d6
KecoaxBunting <david.alvyn@binus.ac.id>
2024-10-15 11:07:41 +0700

    lanjut pembuatan layanan

---
+++ public/js/layanan.js
@@ -0,0 +1,9 @@
+function show() {
+    document.getElementById('show').style.display = "none";
+    document.getElementById('hide').style.display = "block";
+}
+
+function hide() {
+    document.getElementById('show').style.display = "block";
+    document.getElementById('hide').style.display = "none";
+}
--- resources/views/ViewPage/layanan.blade.php
+++ resources/views/ViewPage/layanan.blade.php
@@ -2,37 +2,67 @@
 @section('title', 'Layanan')
 
 @section('content')
-    <div class="container-fluid row pe-0">
-        <div class="left col-md-6 col-12 pe-0">
-            <div class="content bg-primary rounded mb-2">
-                <div class="image p-2 rounded">
+    <script src="{{ asset('js/layanan.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/layanan.css') }}">
+    <div class="container-fluid">
+        <div class="content bg-primary rounded mb-2">
+            <div class="show" id="show">
+                <div class="image p-3 rounded">
                     <img class="img-fluid" src="{{ asset('picture/Gereja.jpg') }}" alt="">
                 </div>
-
-                <div class="bottom p-2 d-flex justify-content-between">
+                <div class="bottom p-3 d-flex justify-content-between">
                     <div class="fs-5 fw-bolder text-white">Judul Layanan</div>
-                    <div class="btn btn-outline-primary bg-white text-primary">
-                        <a href="" class="nav-link">Info lebih lanjut</a>
+                    <div class="btn btn-outline-primary bg-white text-primary" onclick="show()">
+                        Info lebih lanjut
                     </div>
                 </div>
-
+            </div>
+            <div class="hide p-3" id="hide">
+                <div class="head fs-5 mb-2 fw-bolder text-white">Judul Layanan</div>
+                <div class="description fs-6 text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro
+                    provident
+                    labore cupiditate quasi eum obcaecati non, odit delectus! Minima iure ratione culpa corrupti tempora
+                    maiores non quae laudantium in optio.
+                </div>
+                <div class="bottom d-flex justify-content-between">
+                    <div class="btn btn-outline-primary bg-white text-primary" onclick="hide()">
+                        Tutup
+                    </div>
+                    <div class="btn btn-outline-primary bg-white text-primary">
+                        <a href="" class="nav-link">Daftar Sekarang</a>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="right col-md-6 col-12 pe-0">
-            <div class="content bg-primary rounded mb-2">
-                <div class="image p-2 rounded">
+
+        <div class="content bg-primary rounded mb-2">
+            <div class="show" id="show">
+                <div class="image p-3 rounded">
                     <img class="img-fluid" src="{{ asset('picture/Gereja.jpg') }}" alt="">
                 </div>
-
-                <div class="bottom p-2 d-flex justify-content-between">
+                <div class="bottom p-3 d-flex justify-content-between">
                     <div class="fs-5 fw-bolder text-white">Judul Layanan</div>
-                    <div class="btn btn-outline-primary bg-white text-primary">
-                        <a href="" class="nav-link">Info lebih lanjut</a>
+                    <div class="btn btn-outline-primary bg-white text-primary" onclick="show()">
+                        Info lebih lanjut
                     </div>
                 </div>
-
+            </div>
+            <div class="hide p-3" id="hide">
+                <div class="head fs-5 mb-2 fw-bolder text-white">Judul Layanan</div>
+                <div class="description fs-6 text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro
+                    provident
+                    labore cupiditate quasi eum obcaecati non, odit delectus! Minima iure ratione culpa corrupti tempora
+                    maiores non quae laudantium in optio.
+                </div>
+                <div class="bottom d-flex justify-content-between">
+                    <div class="btn btn-outline-primary bg-white text-primary" onclick="hide()">
+                        Tutup
+                    </div>
+                    <div class="btn btn-outline-primary bg-white text-primary">
+                        <a href="" class="nav-link">Daftar Sekarang</a>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
 @endsection
