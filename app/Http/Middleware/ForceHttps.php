e16f3b890e6547a4a311fd8979dd31a46db8b97c
unknown <kevin>
2025-01-13 20:21:44 +0700

    force https for all

---
+++ app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->isSecure() && config('app.env') !== 'local') {
+            // Redirect ke versi HTTPS
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request); // Lanjutkan ke proses berikutnya
+    }
+}
